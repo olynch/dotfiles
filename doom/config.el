52bc483a884e74ad5f8a266150dbcf985b83e088
Owen Lynch <root@owenlynch.org>
2020-04-18 18:58:18 -0400

    automatic update

--- doom/config.el
+++ doom/config.el
@@ -14,11 +14,11 @@
 
 (defun +nix/home-manager-switch ()
   (interactive)
-  (async-shell-command "home-manager switch"))
+  (async-shell-command "update-home"))
 
 (defun +nix/nixos-rebuild-switch ()
   (interactive)
-  (async-shell-command "sudo nixos-rebuild switch"))
+  (async-shell-command "update-nixos"))
 
 (map! :leader
       "r h" '+nix/home-manager-switch
@@ -26,3 +26,26 @@
 
 (set-popup-rules!
   '(("^\\*Async" :slot -1 :modeline f)))
+
+(defun +doom/reload-with-commit ()
+  (interactive)
+  (require 'core-cli)
+  (when (and IS-WINDOWS (file-exists-p doom-env-file))
+    (warn "Can't regenerate envvar file from within Emacs. Run 'doom env' from the console"))
+  ;; In case doom/reload is run before incrementally loaded packages are loaded,
+  ;; which could cause odd load order issues.
+  (mapc #'require (cdr doom-incremental-packages))
+  (doom--if-compile "update-doom"
+      (let ((doom-reloading-p t))
+        (doom-initialize 'force)
+        (with-demoted-errors "PRIVATE CONFIG ERROR: %s"
+          (general-auto-unbind-keys)
+          (unwind-protect
+              (doom-initialize-modules 'force)
+            (general-auto-unbind-keys t)))
+        (run-hook-wrapped 'doom-reload-hook #'doom-try-run-hook)
+        (print! (success "Config successfully reloaded!")))
+    (user-error "Failed to reload your config")))
+
+(map! :leader
+      "h r r" '+doom/reload-with-commit)
--- doom/init.el
+++ doom/init.el
@@ -14,6 +14,8 @@
 ;;      Alternatively, press 'gd' (or 'C-c g d') on a module to browse its
 ;;      directory (for easy access to its source code).
 
+(setq doom-localleader-key ",")
+
 (doom! :input
 
        :completion
