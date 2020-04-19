7782fcb420911bdfb1ae753507078c012e7dc1be
Owen Lynch <root@owenlynch.org>
2020-04-19 00:04:39 -0400

    automatic update

--- home-manager/util/X.nix
+++ home-manager/util/X.nix
@@ -14,7 +14,7 @@
   xdg.configFile."taffybar/taffybar.hs".text = lib.readFile ./taffybar.hs;
   home.file.".xmonad/xmonad.hs".text = lib.readFile ./xmonad.hs;
 
-  services.taffybar.enable = true;
+  # services.taffybar.enable = true;
 
-  services.status-notifier-watcher.enable = true;
+  # services.status-notifier-watcher.enable = true;
 }
