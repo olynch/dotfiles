ea0956e28cb9afd714c2b628c41e5906b6544372
Owen Lynch <root@owenlynch.org>
2020-04-19 16:27:17 -0400

    automatic update

--- home-manager/devel/languages/latex.nix
+++ home-manager/devel/languages/latex.nix
@@ -1,10 +1,6 @@
 { config, lib, pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    dvipng
-  ];
-
   programs.texlive = {
     enable = true;
     extraPackages = tpkgs: { inherit (tpkgs) scheme-medium; };
